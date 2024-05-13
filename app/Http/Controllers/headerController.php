d9dadb35986e7397a515ab40f1184faed40782e2
intratechconsulting <intratechconsulting1@gmail.com>
2024-05-13 02:46:57 +0000

    Finished planning page redirect to the good calendar

--- app/Http/Controllers/headerController.php
+++ app/Http/Controllers/headerController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\RecievePlanningController;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+
 
 
 
@@ -44,7 +46,17 @@ public function home()
 
 public function planning()
     {
-   return view('user.planning');
+
+ if (Auth::check()) {
+        $user = Auth::user(); 
+        $calendar_link = $user->calendar_link;
+    } else {
+        // default calendar link
+        $calendar_link = "https://calendar.google.com/calendar/u/0/embed?src=9ecbb3026111b91a9ce21bfed88d67b95783a5a418c6d82aaa220776eb70f5d3@group.calendar.google.com&ctz=Europe/Brussels";
+    }
+
+    return view('user.planning', ['calendar_link' => $calendar_link]);
+
     }
 
 
--- resources/views/user/planning.blade.php
+++ resources/views/user/planning.blade.php
@@ -10,7 +10,7 @@
 
 <div>
 <center>
-<iframe src="https://calendar.google.com/calendar/embed?src=9ecbb3026111b91a9ce21bfed88d67b95783a5a418c6d82aaa220776eb70f5d3%40group.calendar.google.com&ctz=Europe%2FBrussels" style="border: 0" width="80%" height="600" frameborder="0" scrolling="no"></iframe>
+<iframe src="{{ $calendar_link }}" style="border: 0" width="80%" height="600" frameborder="0" scrolling="no"></iframe>
     </center>
 
 
