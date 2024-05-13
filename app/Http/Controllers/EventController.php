8368a4a14f4e2afdf7f0d9d647019fcf62316177
intratechconsulting <intratechconsulting1@gmail.com>
2024-05-13 14:11:46 +0000

    Pushing existing code to switch to CI/CD

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\RabbitMQSendToExhangeService;
-use App\Models\User;
+use App\Models\Event;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+
+
+
 
 
 
