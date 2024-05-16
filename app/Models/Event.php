60f44196980550a1004ab52491e6de003fc649d6
tom-claes <133092129+tom-claes@users.noreply.github.com>
2024-05-16 14:45:21 +0200

    Uploading models

---
+++ app/Models/Event.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+   use HasFactory;
+
+/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'date',
+        'start_time',
+        'end_time',
+        'location',
+        'max_registrations',
+        'available_seats',
+        'description',
+        'speaker_user_id',
+        'speaker_company_id',
+        // Add any other attributes here that are fillable
+    ];
+}
