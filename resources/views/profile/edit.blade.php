8dc3eb69f0f21950e5604216543df0c50745bae1
Jensyfranck <jensy.franck@student.ehb.be>
2024-05-18 13:12:02 +0200

    debugging delete user

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="//unpkg.com/alpinejs" defer></script>
     <title>Profile</title>
     <style>
         .registration-card {
