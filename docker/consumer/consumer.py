040985069e1128076ad48a76148b0f15a70b396e
zakariyaben10 <133095930+ZakariyaBen10@users.noreply.github.com>
2024-05-19 13:05:24 +0200

    Update consumer hashing password
    Updated the consumer hashing method for the password

--- docker/consumer/consumer.py
+++ docker/consumer/consumer.py
@@ -7,8 +7,8 @@ import bcrypt
 def create_user(user_data):
     try:
         default_password = "azerty123"
-        hashed_password = bcrypt.hashpw(default_password.encode(), bcrypt.gensalt()).decode()
-
+        hashed_password = bcrypt.hashpw(default_password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        
         sql = """INSERT INTO users (id, first_name, last_name, email, telephone, birthday, country, state, city, zip, street, house_number, 
                  company_email, company_id, user_role, invoice, calendar_link, password, created_at, updated_at) 
                  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
@@ -95,7 +95,7 @@ def create_company(company_data):
     try:
         
         default_password = "qwerty123"
-        hashed_password = bcrypt.hashpw(default_password.encode(), bcrypt.gensalt()).decode()
+        hashed_password = bcrypt.hashpw(default_password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
         
 
         sql = """INSERT INTO companies (id, name, email, telephone, logo, country, state, city, zip, street, house_number, type, invoice, user_role, password, created_at, updated_at) 
