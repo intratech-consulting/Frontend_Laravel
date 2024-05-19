b50a7befe4f7ae49db9c454ae7323fd09608ee5a
Jensyfranck <jensy.franck@student.ehb.be>
2024-05-19 16:52:16 +0200

    routings fix

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware('web')->group(function () {
     })->middleware(['auth', 'verified'])->name('user.home');
 
     Route::middleware(['web', 'auth'])->group(function () {
-        Route::put('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
