0d5f9346bcec3d76a0018aafc8bfd1a54bd336ba
Jensyfranck <jensy.franck@student.ehb.be>
2024-05-19 14:12:23 +0200

    debugging CRUD update

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::middleware('web')->group(function () {
 
     Route::middleware(['web', 'auth'])->group(function () {
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
 
