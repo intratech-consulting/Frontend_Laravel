573b19b442969cc787d7b684291bece5c44eea80
Jensyfranck <jensy.franck@student.ehb.be>
2024-05-20 20:31:34 +0200

    Fix redirect issue in AuthenticatedSessionController and web.php

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,11 @@ Route::middleware('web')->group(function () {
 
     Route::post('/send-message-to-topic', [testController::class, 'sendMessageToTopic'])->name('send_message_to_topic');
 
-    Route::get('/home', function () {
-        return view('user.home');
-    })->middleware(['auth', 'verified'])->name('user.home');
+    Route::middleware('web')->group(function () {
+        Route::get('/home', function () {
+            return view('user.home');
+        })->middleware(['auth', 'verified'])->name('user.home');
+    });
 
     Route::middleware(['web'])->group(function () {
         Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
