a7767ee4f5a8809adf162b36137a8127940535f5
Jensyfranck <jensy.franck@student.ehb.be>
2024-05-20 14:26:58 +0200

    login user before fetching ID

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -107,6 +107,8 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
+        Auth::login($user);
+
         // Create a new Guzzle HTTP client
         $client = new \GuzzleHttp\Client();
 
@@ -177,8 +179,6 @@ class RegisteredUserController extends Controller
         $routingKey = 'user.frontend';
 
         $this->sendMessageToTopic($routingKey, $message);
-
-        Auth::login($user);
 
         return redirect()->route('user.home');
     }
