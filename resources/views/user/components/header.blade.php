ec704b1821c8b948aafea6fe6ac4526779c7b979
tom-claes <133092129+tom-claes@users.noreply.github.com>
2024-05-24 11:13:48 +0200

    Fix: messages

--- resources/views/user/components/header.blade.php
+++ resources/views/user/components/header.blade.php
@@ -21,30 +21,6 @@
       box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     }
 
-    .flash-success {
-        padding: 20px;
-        background-color: #33a43a;
-        color: white;
-        margin: 20px 0px;
-        border-radius: 4px;
-    }
-
-    .flash-error {
-        padding: 20px;
-        background-color: rgb(235, 47, 47);
-        color: white;
-        margin: 20px 0px;
-        border-radius: 4px;
-    }
-
-    .flash-remove{
-        padding: 20px;
-        background-color: #FF5733;
-        color: white;
-        margin: 20px 0px;
-        border-radius: 4px;
-    }
-
     .home {
       text-decoration: none;
       color: #fff;
@@ -177,26 +153,5 @@
     </ul>
   </nav>
 </header>
-
-
-<div class="flex-grow ml-56 p-8">
-    @if (session('success'))
-        <div class="flash-success">
-            {{session('success')}}
-        </div>
-    @endif
-
-    @if (session('remove'))
-        <div class="flash-remove">
-            {{session('remove')}}
-        </div>
-    @endif
-
-        @if (session('error'))
-            <div class="flash-error">
-                {{session('error')}}
-            </div>
-        @endif
-</div>
 </body>
 </html>
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -15,6 +15,29 @@
       text-decoration: none;
     }
 
+    .flash-success {
+        padding: 20px;
+        background-color: #33a43a;
+        color: white;
+        margin: 20px 0px;
+        border-radius: 4px;
+    }
+
+    .flash-error {
+        padding: 20px;
+        background-color: rgb(235, 47, 47);
+        color: white;
+        margin: 20px 0px;
+        border-radius: 4px;
+    }
+
+    .flash-remove{
+        padding: 20px;
+        background-color: #FF5733;
+        color: white;
+        margin: 20px 0px;
+        border-radius: 4px;
+    }
 
     .hackathon-container {
       display: flex;
@@ -242,6 +265,26 @@
 <body>
 
 @include('user.components.header')
+
+<div class="flex-grow ml-56 p-8">
+    @if (session('success'))
+        <div class="flash-success">
+            {{session('success')}}
+        </div>
+    @endif
+
+    @if (session('remove'))
+        <div class="flash-remove">
+            {{session('remove')}}
+        </div>
+    @endif
+
+    @if (session('error'))
+        <div class="flash-error">
+            {{session('error')}}
+        </div>
+    @endif
+</div>
 
 <main class="hackathon-container">
 <section class="hero-section">
