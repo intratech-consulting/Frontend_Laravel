12e1abb89432b67dd416c8a3ee66862963b384fc
tom-claes <133092129+tom-claes@users.noreply.github.com>
2024-05-24 16:17:29 +0200

    Fix: index.php

--- public/index.php
+++ public/index.php
@@ -1,26 +1,17 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-if ($app === null) {
-    die('Could not initialize application.');
-}
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
