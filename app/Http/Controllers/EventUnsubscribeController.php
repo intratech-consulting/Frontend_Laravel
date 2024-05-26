5f8d9e53b4cccba0046b5ad318204ffdf1ae359c
jackthyssens <jack.thyssens@student.ehb.be>
2024-05-26 23:15:44 +0200

    debug unsubscribe

--- app/Http/Controllers/EventUnsubscribeController.php
+++ app/Http/Controllers/EventUnsubscribeController.php
@@ -51,8 +51,6 @@ class EventUnsubscribeController extends Controller
                                 ->where('event_id', $eventId)
                                 ->first();
 
-        die($attendance);
-
         $attendanceId = $attendance->id;             
  
         // Create XML message
