0df3710695cf19773dd9eb0e3366f361ff6d124a
Jensyfranck <jensy.franck@student.ehb.be>
2024-05-26 21:20:11 +0200

    refactor: Switch Mijn reservaties with Profile

--- resources/views/user/components/header.blade.php
+++ resources/views/user/components/header.blade.php
@@ -159,8 +159,8 @@
           <li class="user-menu">
             <span class="user-name">{{ Auth::guard('web')->user()->name }}</span>
             <div class="user-actions">
-              <a href="{{ url('/profile') }}">Profile</a>
               <a href="{{ url('/mijnReservaties') }}">Mijn reservaties</a>
+              <a href="{{ url('/profile') }}">Profile</a>
               <form method="POST" action="{{ route('logout') }}">
                 @csrf
                 <button type="submit" class="logout-button">Logout</button>
